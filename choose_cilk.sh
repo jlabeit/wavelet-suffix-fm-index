68613bb818ee069ee6a37c5f80d610e53df2ad24
jlabeit <julianlabeit@gmail.com>
2015-04-19 14:24:56 -0400

    Renamed choose silk script

--- choose_cilk.sh
+++
@@ -1,5 +0,0 @@
-export PATH=/home/julian/gcc-cilk/bin:$PATH
-export CPATH=/home/julian/gcc-cilk/include:$CPATH
-export LIBRARY_PATH=/home/julian/gcc-cilk/lib:/home/julian/gcc-cilk/lib64:$LIBRARY_PATH
-export LD_LIBRARY_PATH=/home/julian/gcc-cilk/lib:/home/julian/gcc-cilk/lib64:$LD_LIBRARY_PATH
-export GCILK=true
