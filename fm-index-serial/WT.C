afa7ae06514d4c5418dd43ddbcdbd2205295debe
Julian Labeit <julianlabeit@gmail.com>
2017-01-03 00:05:31 +0100

    Added sequential version of fm-index.

---
+++ fm-index-serial/WT.C
@@ -0,0 +1,44 @@
+// This code is part of the paper "Parallel Wavelet Tree Construction"
+// Copyright (c) 2014 Julian Shun
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights (to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#include <divsufsort.h>
+#include "WT.h"
+#include "parallel.h"
+#include "utils.h"
+#include <iostream>
+#include "sequence.h"
+
+
+#define THRESHOLD 10000
+
+
+void FMIndex(symbol* s, int32_t* data, long n) {
+	// Calculate SA
+    divsufsort(s, (int32_t*)data, n);
+	sdsl::int_vector<sizeof(symbol)*8> bwt(n);
+	// Calculate BWT
+	int32_t to_add[2] = {(int32_t)-1,(int32_t)n-1};
+        for (long i=0; i < n; ++i) {
+		bwt[i] = s[data[i]+to_add[data[i]==0]];
+	}
+	// Construct WT
+	sdsl::wt_huff<> wt(bwt,n);
+}
