b44f29bf945371cfcb740f21329b747d5afd07c8
Julian Labeit <julianlabeit@gmail.com>
2017-02-06 10:04:35 +0100

    Added some scripts.

---
+++ fm-index/buildTimingTable.py
@@ -0,0 +1,80 @@
+import csv
+
+# Read result data.
+def parseRow(row):
+    return [row[0].strip(), row[1].strip(), int(row[2]), int(row[3]), float(row[4])]
+
+data = []
+with open('results', 'rb') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for row in spamreader:
+        data.append(parseRow(row))
+
+artificial = ('artificial repetitive', ['fib41', 'rs.13', 'tm29'])
+real = ('real repetitive', ['Escherichia_Coli', 'cere', 'coreutils', 'einstein.de.txt',
+        'influenza', 'kernel', 'para', 'world_leaders'])
+pseudo= ('pseudo repetitive', ['dblp.xml.00001.1', 'dblp.xml.00001.2', 'dblp.xml.0001.1',
+    'dblp.xml.0001.2', 'dna.001.1', 'english.001.2', 'proteins.001.1', 'sources.001.2'])
+classic = ('non-repetitive', ['sources', 'pitches', 'proteins', 'dna', 'english.1024MB', 'dblp.xml'])
+special = ('special cases', ['aaa', 'abab', 'aabbaabb'])
+
+inputs = [classic, real, artificial, pseudo, special]
+#threads = [1, 2, 4, 8, 12, 16, 24, 32, 40, 48, 56, 64]
+threads = [1, 8, 16, 32, 64]
+
+salgos = ['ser']
+palgos = ['par'] 
+
+
+result_file = open('resultsFMIndex.tex', 'w')
+
+def getTime(a, b, c):
+    for row in data:
+        if row[0] == a and row[1] == b and row[2] == c:
+            return row[4]
+    return 1.0;
+
+def write_label(l):
+    result_file.write('\\multicolumn{2}{l}{\\textit{' + label + '}}\\\\ \n')
+
+def getTiming(f, algo, fastest):
+    if (algo in salgos):
+        return '%.2f '% getTime(algo, f, 1)
+    else:
+        t1 = getTime(algo, f, 1)
+        t64 = getTime(algo, f, 64)
+        if (fastest):
+            return '%.2f & \\textbf{%.2f} & %.2f &' % (t1, t64, t1 / t64) 
+        return '%.2f & %.2f & %.2f &' % (t1, t64, t1 / t64) 
+
+def getFastest(f):
+    mini = 100000000.0;
+    result = ''
+    for algo in palgos:
+        cur = getTime(algo, f, 64)
+        if mini > cur:
+            mini = cur
+            result = algo
+    return result
+
+def write_result(f):
+    result_file.write(f.replace('_', '\\_') + ' & ')
+    fastest = getFastest(f)
+    for algo in palgos:
+        result_file.write(getTiming(f, algo, fastest == algo))
+    for algo in salgos:
+        result_file.write(getTiming(f, algo, fastest == algo))
+    result_file.write('\\\\\n')
+    
+
+result_file.write('\\begin{tabular}{l | r r r | r}\n')
+
+result_file.write('Input & $T_1$ & $T_{64}$ & $\\frac{T_1}{T_{64}}$ & $T_1$ \\\\\n')
+
+result_file.write('\hline\n')
+for (label, files) in inputs:
+    write_label(label)
+    for f in files:
+        write_result(f)
+result_file.write('\\bottomrule\n')
+result_file.write('\\end{tabular}\n')
---
+++ fm-index/plot.py
@@ -0,0 +1,66 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+
+# Read result data.
+def parseRow(row):
+    return [row[0].strip(), row[1].strip(), int(row[2]), int(row[3]), float(row[4])]
+
+data = []
+with open('results', 'rb') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for row in spamreader:
+        data.append(parseRow(row))
+
+salgos = ['ser']
+palgos = [('black', '|' , 'par')]
+
+def getTime(algo, f, p):
+    for row in data:
+        if row[0] == algo and row[1] == f and row[2] == p:
+            return row[4]
+    return 1.0;
+
+
+
+def getSpeedup(algo, f):
+    return [getTime('ser', f, 1) / getTime(algo, f, p) for p in threads]
+lines = dict()
+
+def makePlot(plot, filename):
+    for (c, m, algo) in palgos:
+        speedup = getSpeedup(algo, filename)
+        lines[algo], = plot.plot(threads, speedup, c, marker=m, label=algo)
+    plot.set_xticks([1, 4, 8, 12, 16, 24, 32, 40, 48, 56, 64])
+    plot.set_title(filename.replace('_', '\\_'), fontsize=16)
+
+# Example data
+threads = [1, 2, 4, 8, 12, 16, 24, 32, 40, 48, 56, 64]
+
+f, ((ax1, ax2), (ax3, ax4), (ax5, ax6), (ax7, ax8), (ax9, ax10)) = plt.subplots(5, 2, sharex='col', sharey='row')
+
+makePlot(ax1, 'dna')
+makePlot(ax2, 'sources')
+makePlot(ax3, 'cere')
+makePlot(ax4, 'world_leaders')
+makePlot(ax5, 'tm29')
+makePlot(ax6, 'fib41')
+makePlot(ax7, 'proteins.001.1')
+makePlot(ax8, 'dna.001.1')
+makePlot(ax9, 'proteins.001.1')
+makePlot(ax10, 'dna.001.1')
+ax1.set_ylabel(r'absolute speedup', fontsize=16)
+ax3.set_ylabel(r'absolute speedup', fontsize=16)
+ax5.set_ylabel(r'absolute speedup', fontsize=16)
+ax7.set_ylabel(r'absolute speedup', fontsize=16)
+ax9.set_ylabel(r'absolute speedup', fontsize=16)
+ax9.set_xlabel(r'number of threads', fontsize=16)
+ax10.set_xlabel(r'number of threads', fontsize=16)
+
+
+f.legend([lines['par']], ['par'], 'lower center', ncol=4)
+
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+plt.savefig('fmindexPlot.eps', format='eps')
+plt.show()
